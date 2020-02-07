14c89d014615d6c01870f8647e4fcae7752f0f37
jmczerwinska <asia.frej@gmail.com>
2020-02-07 15:12:06 +0100

    style components

--- src/components/MakePizza/MakePizza.jsx
+++ src/components/MakePizza/MakePizza.jsx
@@ -17,7 +17,7 @@ function MakePizza(props) {
 
   useEffect(() => {
     setPrice(ingredients.reduce((sum, ingredient) => {
-      return ingredient.checked ? sum +ingredient.price : sum;
+      return ingredient.checked ? sum + ingredient.price : sum;
     }, base))
   }, [base, ingredients])
 
@@ -50,14 +50,19 @@ function MakePizza(props) {
     }
 
     ingredients.forEach((el) => {if (el.checked) pizza.ingredients.push(el);})
-    console.log(pizza.ingredients)
     props.pizzaSender(pizza);
+    resetPizza();
+  }
 
+
+  const resetPizza = () => {
+    ingredients.forEach(el => el.price === 0 ? el.checked = true : el.checked = false);
+    setBase(800);
   }
 
 	return (
-		<div className="pizzaMaker">
-			<h1>Skomponuj własną pizzę</h1>
+		<div className="pizza-maker">
+			<h2>Skomponuj własną pizzę</h2>
 			<h4>Cena: {(price / 100).toFixed(2)} zł</h4>
 			
       <div className="selectSize">
@@ -85,7 +90,7 @@ function MakePizza(props) {
     	<div className="ingredients">
       	{ ingredients.map((ingredient, i) => {
       	  return (
-          	<div className="ingredient" key={i}>
+          	<div className="ingredient-row" key={i}>
             	<input type="checkbox" checked={ingredient.checked} onChange={() => ingredientChange(ingredient)} />
             	<p>{ingredient.name}</p>
 							{ ingredient.price ===  0 ? <p>FREE</p> : <p>{(ingredient.price / 100).toFixed(2)} zł</p> }
