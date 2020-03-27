9c81bef54314eba2403210c062ce8f4d1da42c52
jmczerwinska <asia.frej@gmail.com>
2020-03-27 15:14:01 +0100

    render basket content in order component

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -6,26 +6,63 @@ import Price from '../UI/Price';
 
 
 
-function Order (props) {
+function Order(props) {
+    const fullPrice = (context) => context.basket.reduce((sum, pizza) => sum + pizza.price, 0);
 
-const backToMenu = () => {
-    props.history.push("/");
-}
+
+
+
+    const showIngredients = (pizza) => {
+        let nameArr = [];
+        pizza.ingredients.forEach(el => nameArr.push(el.name));
+        const ingredientsList = nameArr.join(', ')
+        return ingredientsList;
+    }
+
+    const removePizza = (context, i) => {
+        const updateBasket = context.basket.filter((pizza, index) => index !== i);
+        context.refresh(updateBasket);
+    }
+
+    const backToMenu = () => {
+        props.history.push("/menu");
+    }
+
+    const nextStep = () => {props.history.push("/order/delivery")};
 
     return (
         <ContextConsumer>
-            
+
             {context => (
                 <div>
-                    <h1>Zamówienie:</h1>
-                   {console.log(context.basket)}
-                    <Button title="Wróć do zamówienia" onSubmit={backToMenu} />   
+                    <h2>Zamówienie:</h2>
+                    {
+                        context.basket.map((pizza, i) => {
+                            return (
+                                <div key={i} className="basket-row">
+                                    <div className="basket-head">
+                                        <h4>
+                                            {i + 1}# {pizza.size} pizza - kompozycja własna
+                                        &nbsp; | &nbsp;
+                                        <Price price={pizza.price} />
+                                        </h4>
+                                        <button onClick={() => removePizza(context, i)}>&times;</button>
+                                    </div>
+                                    <p className="basket-info">Dodatki: {showIngredients(pizza)}</p>
+                                </div>
+                            )
+                        })
+                    }
+                    <h3>Do zapłaty: <Price price={fullPrice(context)} /></h3>
+                    <Button title="Zamów" onSubmit={() => nextStep()} />
+                    {console.log(context.basket)}
+                    <Button title="Wróć do zamówienia" onSubmit={backToMenu} />
                 </div>
-                
+
             )}
-            
+
         </ContextConsumer>
-        
+
     )
 }
 
