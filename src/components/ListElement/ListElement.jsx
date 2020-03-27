bd78d7de724d1af30efaf0a5b379ee230951d0d6
jmczerwinska <asia.frej@gmail.com>
2020-03-27 17:14:38 +0100

    add new component

---
+++ src/components/ListElement/ListElement.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Price from '../UI/Price';
+import { ContextConsumer } from '../..';
+
+function ListElement({pizza, i}) {
+    const [selected, setSelected] = useState(['średnia']);
+
+    const addPizza = context => {
+        const newPizza = {
+            name: pizza.name,
+            size: selected,
+            price: findPrice()
+        };
+        context.basket.push(newPizza);
+        console.log(context.basket);
+        setSelected('średnia');
+    }
+    const findPrice = () => {
+        switch (selected) {
+            case 'mała':
+                return pizza.price.s;
+            case 'duża':
+                return pizza.price.l;
+            default:
+                return pizza.price.m;
+        }
+    }
+
+    return (
+        <li key={i}>
+            <h4>{pizza.name}</h4>
+            <p>{pizza.ingredients.join(', ')}</p>
+            <select value={selected} onChange={e => setSelected(e.target.value)}>
+                <option value='mała'> Mała </option>
+                <option value='średnia'>Średnia</option>
+                <option value='duża'>Duża</option>
+            </select>
+            <p>Cena: <Price price={findPrice()} /></p>
+            <ContextConsumer>
+                {context => (
+                   <button onClick={() => addPizza(context)}>Dodaj</button> 
+                )}
+            </ContextConsumer>
+            
+        </li>
+    )
+}
+
+export default ListElement;
