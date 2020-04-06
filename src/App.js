83010de260ef756fbf9f10179e309e722a940219
jmczerwinska <asia.frej@gmail.com>
2020-04-06 15:31:10 +0200

    add Footer component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Order from './components/Order/Order';
 import Menu from './components/Menu/Menu';
 import MakePizza from './components/MakePizza/MakePizza';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/compose" component={MakePizza}/>
         <Route path="/order" component={Order} />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p> Copyrigth© Joanna Czerwińska 2020</p>
+            <p>Foto:</p>
+        </footer>
+    )
+}
+
+export default Footer;
