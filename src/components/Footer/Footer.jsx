7c7050e601e2a8655b2bc358d19c8cb22306907b
jmczerwinska <asia.frej@gmail.com>
2020-04-06 20:37:55 +0200

    style Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Footer.scss';
 
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <p> Copyrigth© Joanna Czerwińska 2020</p>
-            <p>Foto:</p>
+            <p>Foto<a href="https://pixabay.com/pl/users/zuzi99-7340598/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3010062"> zuzana gazdikova</a> z <a href="https://pixabay.com/pl/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3010062"> Pixabay</a></p>
         </footer>
     )
 }
