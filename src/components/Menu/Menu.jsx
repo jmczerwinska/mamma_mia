e8385a7384ddfa2b19610ad631c6d5da95a1fe3c
jmczerwinska <asia.frej@gmail.com>
2020-04-10 19:58:00 +0200

    mobile view for menu page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -15,7 +15,6 @@ function Menu() {
 
     return (
         <div className="container">
-            <h2>Menu</h2>
             <PizzaList list={rossa} title="Pizza Rossa" />
             <PizzaList list={bianca} title="Pizza Bianca"/>
             <p>Nie zanalazłeś pizzy na jaka masz ochotę? <Link to="/compose">Skomponuj własną!</Link></p>  
--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ListElement from '../ListElement/ListElement';
+import ornament from './ornament.png';
+import './PizzaList.scss';
+
 
 function PizzaList({ list, title }) {
 
     return (
         <div className="category">
-            <h3 className="category__name">{title}</h3>
+            <div  className="title">
+                <img 
+                className="title__ornament" 
+                src={ornament} 
+                alt="ornament" />
+                <h3 className="title__name">{title}</h3>
+            </div>
+
+
             <ol className="menu">
                 {list.map((pizza, i) => <ListElement pizza={pizza} key={i} />)}
             </ol>
