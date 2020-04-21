6724791448e218258745b8786a1338458b88a8df
jmczerwinska <asia.frej@gmail.com>
2020-04-21 16:48:52 +0200

    style header and included components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 
 // import Logo from './Logo.png';
 import Navbar from '../Navbar/Navbar';
-import { Link } from 'react-router-dom';
-import Logo from './logo-mini.png';
 import './Header.scss';
 
 function Header() {
 
     return (
         <header className="header">
-            <Link to="/">
-                <img src={Logo} alt="home link" />
-            </Link>
-
-
             <Navbar />
         </header>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,29 +4,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import { ContextConsumer } from '../..';
 import HamburgerBtn from '../HamburgerBtn/HamburgerBtn';
+import Logo from './logo-mini.png';
 import './Navbar.scss';
 
 function Navbar() {
 
     return (
         <nav className='navbar'>
+            <NavLink to="/">
+                <img src={Logo} alt="home link" />
+            </NavLink>
             <HamburgerBtn />
             <div>
                 <NavLink to="/menu">Menu</NavLink>
                 <NavLink to="/compose">Skomponuj</NavLink>
-                {/* <NavLink to="/contact">Kontakt</NavLink> */}
-                <NavLink to="/order/basket">
-                    <FontAwesomeIcon icon={faShoppingBasket} />
-                    <ContextConsumer>
-                        {context => context.basket.length > 0
-                            ? <span className="basket-counter">{context.basket.length}</span>
-                            : null
-                        }
-                    </ContextConsumer>
-
-                </NavLink>
+                <NavLink to="/contact">Kontakt</NavLink>
             </div>
-
+            <NavLink to="/order/basket">
+                <FontAwesomeIcon icon={faShoppingBasket} />
+                <ContextConsumer>
+                    {context => context.basket.length > 0
+                        ? <span className="basket-counter">{context.basket.length}</span>
+                        : null
+                    }
+                </ContextConsumer>
+            </NavLink>
         </nav>
     )
 }
