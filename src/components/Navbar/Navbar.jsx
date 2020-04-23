b255ab7581124d3b05e1131cce28719549cd9ca1
jmczerwinska <asia.frej@gmail.com>
2020-04-23 22:47:23 +0200

    refactoring code,  fix scroll useEffect

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,14 +10,16 @@ import './Navbar.scss';
 
 function Navbar() {
     const [showNavigation, setShowNavigation] = useState(false);
+    const [additionalClass, setAdditionalClass] = useState('');
 
     const toggleNavigation = () => {
         !showNavigation ? setShowNavigation(true) : setShowNavigation(false);
+        setAdditionalClass(additionalClass === '' ? ' hamburger--active' : '');
     }
 
     useEffect(() => {
-        showNavigation 
-        ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset'
+        showNavigation
+            ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset'
     }, [showNavigation]);
 
     const [scroll, setScroll] = useState(0)
@@ -30,10 +32,10 @@ function Navbar() {
                 setScroll(scrollCheck)
             }
         })
-    })
+    }, [scroll])
 
     return (
-        <nav className={`navbar${scroll ? ' navbar--scroll': ''}`}>
+        <nav className={`navbar${scroll ? ' navbar--scroll' : ''}`}>
             <div className="navbar__logo">
                 <NavLink className="navigation__link" to="/" >
                     <img src={Logo} alt="home link" />
@@ -41,35 +43,46 @@ function Navbar() {
             </div>
 
             <BasketBtn />
-            <HamburgerBtn toggleMenu={toggleNavigation} />
+            <HamburgerBtn toggleMenu={toggleNavigation} activeClass={additionalClass} />
             <Slide down
                 when={showNavigation}
                 duration={500}
                 collapse>
                 <div className="navigation">
-                    <NavLink
-                        className="navigation__link"
-                        activeClassName="navigation__link--active"
-                        to="/menu"
-                        aria-current="page"
-                        onClick={toggleNavigation}>
-                        Menu
-                    </NavLink>
-                    <NavLink
-                        className="navigation__link" activeClassName="navigation__link--active"
-                        to="/compose"
-                        aria-current="page"
-                        onClick={toggleNavigation}>
-                        Skomponuj
-                    </NavLink>
-                    <NavLink
-                        className="navigation__link"
-                        activeClassName="navigation__link--active"
-                        to="/contact"
-                        aria-current="page"
-                        onClick={toggleNavigation}>
-                        Kontakt
-                    </NavLink>
+                    <ul className="navigation__list">
+                        <li>
+                            <NavLink
+                                className="navigation__link"
+                                activeClassName="navigation__link--active"
+                                to="/menu"
+                                aria-current="page"
+                                onClick={toggleNavigation}>
+                                Menu
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className="navigation__link" activeClassName="navigation__link--active"
+                                to="/compose"
+                                aria-current="page"
+                                onClick={toggleNavigation}>
+                                Skomponuj
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className="navigation__link"
+                                activeClassName="navigation__link--active"
+                                to="/contact"
+                                aria-current="page"
+                                onClick={toggleNavigation}>
+                                Kontakt
+                            </NavLink>
+                        </li>
+
+
+                    </ul>
+
                 </div>
             </Slide>
         </nav>
