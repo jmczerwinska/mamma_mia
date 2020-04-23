234bd05f3b7598d39581d97fbcd84cfb209d7d84
jmczerwinska <asia.frej@gmail.com>
2020-04-23 12:02:15 +0200

    show and hide menu on click

--- src/components/HamburgerBtn/HamburgerBtn.jsx
+++ src/components/HamburgerBtn/HamburgerBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './HamburgerBtn.scss';
 
-function HamburgerBtn() {
+function HamburgerBtn({ toggleMenu }) {
     return (
-        <button className="hamburger">
+        <button className="hamburger" onClick={toggleMenu}>
             <span className="hamburger__box">
                 <span className="hamburger__line first"></span>
                 <span className="hamburger__line"></span>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import HamburgerBtn from '../HamburgerBtn/HamburgerBtn';
@@ -8,20 +8,53 @@ import Logo from './logo-mini.png';
 import './Navbar.scss';
 
 function Navbar() {
+    const [showNavigation, setShowNavigation] = useState(false);
+
+    const toggleNavigation = () => {
+        showNavigation === false ? setShowNavigation(true) : setShowNavigation(false);
+    }
 
     return (
         <nav className="navbar">
-            <NavLink to="/" className="navbar__logo">
-                <img src={Logo} alt="home link" />
-            </NavLink>
-            <BasketBtn />
-            <HamburgerBtn />
-            <div className="navbar__menu">
-                <NavLink to="/menu">Menu</NavLink>
-                <NavLink to="/compose">Skomponuj</NavLink>
-                <NavLink to="/contact">Kontakt</NavLink>
+            <div className="navbar__logo">
+                <NavLink to="/" >
+                    <img src={Logo} alt="home link" />
+                </NavLink>
             </div>
-            
+
+            <BasketBtn />
+            <HamburgerBtn toggleMenu={toggleNavigation} />
+            {
+                showNavigation === false
+                    ? null
+                    : (<div className="navigation">
+                        <NavLink
+                            className="navigation__link"
+                            activeClassName="navigation__link--active"
+                            to="/menu"
+                            aria-current="page"
+                            onClick={toggleNavigation}>
+                            Menu
+                        </NavLink>
+                        <NavLink
+                            className="navigation__link" activeClassName="navigation__link--active"
+                            to="/compose"
+                            aria-current="page"
+                            onClick={toggleNavigation}>
+                            Skomponuj
+                        </NavLink>
+                        <NavLink
+                            className="navigation__link"
+                            activeClassName="navigation__link--active"
+                            to="/contact"
+                            aria-current="page"
+                            onClick={toggleNavigation}>
+                            Kontakt
+                        </NavLink>
+                    </div>)
+            }
+
+
         </nav>
     )
 }
