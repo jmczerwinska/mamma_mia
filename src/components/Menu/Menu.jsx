a8197867482e51a9abf954fb4e606628e49872c1
jmczerwinska <asia.frej@gmail.com>
2020-04-24 11:00:52 +0200

    add scroll effect, class names and styles

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import './Menu.scss';
+import React, { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
 import menuCart from '../../data/menu';
-import PizzaList from '../PizzaList/PizzaList'
-import { Link } from 'react-router-dom';
+import PizzaList from '../PizzaList/PizzaList';
 
+import './Menu.scss';
 
-function Menu() {
+function Menu({ history }) {
+    useEffect(() => window.scrollTo(0,0), []);
+
     const bianca = [];
     const rossa = [];
     menuCart.map(el => el.category === 'rossa' ? rossa.push(el) : bianca.push(el));
 
-    //TODO dodawanie pizzy do koszyka
-    
-
     return (
         <div className="container">
             <PizzaList list={rossa} title="Pizza Rossa" />
-            <PizzaList list={bianca} title="Pizza Bianca"/>
-            <p>Nie zanalazłeś pizzy na jaka masz ochotę? <Link to="/compose">Skomponuj własną!</Link></p>  
+            <PizzaList list={bianca} title="Pizza Bianca" />
+            <div className="redirection">
+                <p className="redirection__description" >Nie zanalazłeś pizzy na jaką masz ochotę?</p>
+                <button className="redirection__button"onClick={() => history.push('/compose')}>
+                    Skomponuj własną!
+                </button>
+            </div>
+
         </div>
 
     )
 }
 
-export default Menu;
+export default withRouter(Menu);
