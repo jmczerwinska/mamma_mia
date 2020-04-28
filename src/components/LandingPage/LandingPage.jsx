2cec5d98c4082ce0ac84cccc1472b4429c401934
jmczerwinska <asia.frej@gmail.com>
2020-04-28 22:22:13 +0200

    change class names

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -9,9 +9,9 @@ function Start({ history }) {
 
     return (
         <div className="start-page">
-            <div className="title">
-                <img className="title__logo" src={Logo} alt="logo" />
-                <h3 className="title__subtitle">Włoska pizza w Twoim domu</h3>
+            <div className="hero">
+                <img className="hero__logo" src={Logo} alt="logo" />
+                <h3 className="hero__subtitle">Włoska pizza w Twoim domu</h3>
             </div>
             <div className="btn-group">
                 <button className="btn-group__button" onClick={() => history.push('/menu')}>Menu</button>
