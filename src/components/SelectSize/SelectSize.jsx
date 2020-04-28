e931a89d42a2c904f2e83a5ffc15527d2755df33
jmczerwinska <asia.frej@gmail.com>
2020-04-28 22:27:56 +0200

    refactor code, style component

--- src/components/SelectSize/SelectSize.jsx
+++ src/components/SelectSize/SelectSize.jsx
@@ -1,37 +1,39 @@
 import React, { useState } from 'react';
 
+import './SelectSize.scss';
+
 function SelectSize(props) {
+    const sizes = [
+        {name: "Mała", size: 600},
+        {name: "Średnia", size: 800},
+        {name: "Duża", size: 1000}
+    ]
     const [base, setBase] = useState(800);
-    
-    const addClass = (size) => base === size ? " selected" : "";
-    
+
+    const addClass = (size) => base === size ? " size--selected" : "";
+
     const changeSize = size => {
         setBase(size);
-        props.sizeSender(size);        
+        props.sizeSender(size);
     }
 
     return (
-        <div className="selectSize">
+        <div className="select-size">
             <h4>Wybierz rozmiar</h4>
-            <img
-                className={"size small" + addClass(600)}
-                onClick={() => changeSize(600)}
-                src={`${process.env.PUBLIC_URL}assets/size.png`}
-                alt="mała pizza" />
-            <p>Mała</p>
-            <img
-                className={"size medium" + addClass(800)}
-                onClick={() => changeSize(800)}
-                src={`${process.env.PUBLIC_URL}assets/size.png`}
-                alt="średnia pizza" />
-            <p>Średnia</p>
-            <img
-                className={"size large" + addClass(1000)}
-                onClick={() => changeSize(1000)}
-                src={`${process.env.PUBLIC_URL}assets/size.png`}
-                alt="duża pizza" />
-            <p>Duża</p>
+            <div className="sizes">
+                {sizes.map(el => (
+                    <section key={el.size}>
+                        <img
+                            className={`size size--${el.size}` + addClass(el.size)}
+                            onClick={() => changeSize(el.size)}
+                            src={`${process.env.PUBLIC_URL}assets/size.png`}
+                            alt={`${el.name} pizza`} />
+                        <p className="size__name">{el.name}</p>
+                    </section>
+                ))}
+            </div>
         </div>
+
     )
 }
 
