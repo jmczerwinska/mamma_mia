c149044520984067c8658ec18ad11b1883269671
jmczerwinska <asia.frej@gmail.com>
2020-05-04 17:02:16 +0200

    small style changes

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -6,7 +6,7 @@ import './Summary.scss';
 function Summary() {
     return (
         <div className="summary">
-            <p className="summary__info">Dziękujemy za złożenie zamówienia!</p>
+            <h3 className="summary__info">Dziękujemy za złożenie zamówienia!</h3>
             <Timer deliveryTime={124} />
         </div>
     )
