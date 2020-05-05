14bbc8bf877a781bdd09a3a47eb006edd65cf8d2
jmczerwinska <asia.frej@gmail.com>
2020-05-05 14:09:08 +0200

    Updates

---
+++ precache-manifest.bcfa8434fc8576e456a43fe045fcaf1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055fa612fd3026b4dd59d0ea8daaf63e",
+    "url": "/mamma_mia/index.html"
+  },
+  {
+    "revision": "10b5ebc5eeb5e7d6ab79",
+    "url": "/mamma_mia/static/css/main.30ef83d5.chunk.css"
+  },
+  {
+    "revision": "686b0095c214c30752ee",
+    "url": "/mamma_mia/static/js/2.33a8a3a7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mamma_mia/static/js/2.33a8a3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b5ebc5eeb5e7d6ab79",
+    "url": "/mamma_mia/static/js/main.54eae802.chunk.js"
+  },
+  {
+    "revision": "d728b2a1bc9d6fdaf2ad",
+    "url": "/mamma_mia/static/js/runtime-main.f362cada.js"
+  },
+  {
+    "revision": "a0c50c9b1397e2e9354aeb12cc16893c",
+    "url": "/mamma_mia/static/media/grafika.a0c50c9b.png"
+  },
+  {
+    "revision": "925698dee8e9a0648e3644c17abc1c30",
+    "url": "/mamma_mia/static/media/logo.925698de.png"
+  },
+  {
+    "revision": "a699bf98c4d84df859f82f40936f53f3",
+    "url": "/mamma_mia/static/media/ornament.a699bf98.png"
+  }
+]);
