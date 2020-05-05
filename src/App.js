b92732da59a6c78900b876ae7586b6cb0be929f2
jmczerwinska <asia.frej@gmail.com>
2020-05-05 13:52:20 +0200

    comment footer import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 
 function App({ children }) {
   return (
