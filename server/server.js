a96b734fcdeabc3c028e92bdf79b01ac03b1e51a
jmczerwinska <czerwinska.jm@gmail.com>
2021-06-17 16:53:06 +0200

    conect app with MongoDB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB =require('./config/db')
+//Route files
+const menu = require("./routes/menu");
 
 //add env vars
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
+
+app.use('/api/v1/menu', menu)
+
 
 const PORT = process.env.PORT || 5000;
 
