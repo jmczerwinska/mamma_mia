87d160e6d92771da994356570d07fd68bb03c7f2
jmczerwinska <czerwinska.jm@gmail.com>
2021-06-25 13:10:02 +0200

    handling mongoose errors

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -5,13 +5,25 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
     
     //Log to the console for dev
-    console.log(err.stack.brightRed);
+    console.log(err);
 
     //Mongoose bad ObcjectId
-    if (err.name === "CastError") {
+    if (err.name === 'CastError') {
         const message = `Pizza not find with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    };
+
+    //Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
     }
+
+    //Mongoose valitaion error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400)
+    };
 
     res.status(error.statusCode || 500).json({
         success: false,
