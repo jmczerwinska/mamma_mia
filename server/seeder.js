9ae0502409d58fff7522eed72d3d8a48287caa7a
jmczerwinska <czerwinska.jm@gmail.com>
2021-07-12 12:46:14 +0200

    create data seeder

---
+++ server/seeder.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: "./config/config.env" });
+
+const Menu = require('./models/Menu');
+const asyncHandler = require('./middleware/async');
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+});
+
+const menu = JSON.parse(fs.readFileSync(`${__dirname}/_data/menu.json`), 'utf-8');
+
+//Import into DB
+const importData = asyncHandler( async(req, res, next) => {
+        await Menu.create(menu);
+
+        console.log('Data imported...'.green.inverse);
+        process.exit();
+});
+
+//Delete from DB
+const destroyData = asyncHandler( async (req, res, next) => {
+        await Menu.deleteMany();
+
+        console.log('Data destroyed..'.red.inverse);
+        process.exit();
+});
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    destroyData();
+}
