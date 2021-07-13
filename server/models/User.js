e37bc954c79622fdc198e339b41de97f86277b77
jmczerwinska <czerwinska.jm@gmail.com>
2021-07-13 17:21:08 +0200

    create new mongoose model for user

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = required('mongoose');
+
+const User = mongoose.Schema({
+     email: {
+        type: String,
+        reqired: [true, 'Please add an e-mail'],
+        match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, 'The e-mail adress is incorrect'],
+        unique: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'staff'],
+        required: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: [8, 'Password can not be shorter than 8 characters'],
+        select: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: string,
+        required: [true, 'Please add name'],
+    },
+    lastName: {
+        type: string,
+        required: [true, 'Please add name'],
+    }
+});
