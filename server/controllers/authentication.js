39637b61c90a8f62d4810a26498d480e2abb0da6
jmczerwinska <czerwinska.jm@gmail.com>
2021-07-16 15:45:00 +0200

    add user to db with encryped password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,11 +1,23 @@
 const asyncHandler = require("../middleware/async");
+const User = require('..//models/User');
+
+
 //@desc     Add new ingredient
 //@route    POST /api/v1/ingredients
-//@access   Private
-
+//@access   Public
 exports.register = asyncHandler(async (req, res, next) => {
+    const { email, password, name, lastName, role } = req.body;
+    console.log(req.body)
+
+    const user = await User.create({
+        email,
+        password,
+        name,
+        lastName,
+        role
+    });
     
     res.status(201).json({ 
-        success: true, 
+        success: true
     });  
 });
--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.Schema({
+const UserSchema = mongoose.Schema({
      email: {
         type: String,
         reqired: [true, 'Please add an e-mail'],
-        match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, 'The e-mail adress is incorrect'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
+            'Please add a valid e-mail'
+        ],
         unique: true,
         trim: true,
     },
@@ -34,3 +38,12 @@ const User = mongoose.Schema({
         required: [true, 'Please add name'],
     }
 });
+
+//Encrypt password using bcrypt
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+})
+
+module.exports = mongoose.model('User', UserSchema);
