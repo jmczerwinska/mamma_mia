8d1225841c5152f5c7fe0ca08fce06efa49c6bc5
jmczerwinska <czerwinska.jm@gmail.com>
2021-09-21 20:00:26 +0200

    create reveiw model

---
+++ server/models/Review.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = mongoose.Schema({
+    title: {
+        type: String,
+        maxlength:[30, 'Title can not be longer than 30 characters.'],
+        trim: true
+    },
+    raiting: {
+        type: Number,
+        min: 1,
+        max: 10,
+        required: [true, 'Please add a raiting.']
+    },
+    text: {
+        type: String,
+        maxlength: [300, 'Review can not be longer than 300 characters.'],
+        required: [true, 'Please write rewiev.'],
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
