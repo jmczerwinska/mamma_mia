560e07b65969741a8a269bf5d0545fd6ff1855fb
jmczerwinska <czerwinska.jm@gmail.com>
2021-09-22 14:12:27 +0200

    get single review from db

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/')
     .get(getReviews);
+
+router.route('/:id')
+    .get(getReview);
 
 module.exports = router;
 
