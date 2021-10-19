3181a87c39b392c67c5d8519babb0d3a8533e77e
jmczerwinska <czerwinska.jm@gmail.com>
2021-10-19 23:30:23 +0200

    middleware for advanced quering

---
+++ server/middleware/advancedResults.js
@@ -0,0 +1,67 @@
+const advancedResults = (model, populate) => async (req, res, next) => {
+    let query;
+
+    const reqQuery = { ...req.query };
+
+    const removedFields = ['select', 'sort', 'page', 'limit']
+    removedFields.forEach(field => delete reqQuery[field]);
+
+    let queryString = JSON.stringify(reqQuery);
+    queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = model.find(JSON.parse(queryString));
+
+    if(req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 50;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await model.countDocuments();
+    
+    const pagination = {};
+    
+    if(endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        };
+    }
+
+    if(startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        };
+    }
+
+    query = query.skip(startIndex).limit(limit);
+
+    if(populate) {
+        query = query.populate(populate);
+    }
+
+    const results = await query;
+
+    res.advancedResults = {
+        success: true,
+        count: results.length,
+        pagination,
+        data: results
+    };
+    
+    next();
+}
+
+module.exports = advancedResults;
