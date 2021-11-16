58cec6664450766260b65978eac6380c3d04c407
jmczerwinska <czerwinska.jm@gmail.com>
2021-11-16 21:29:48 +0100

    fetch menu from server

--- client/src/components/PizzaMenu/PizzaMenu.jsx
+++ client/src/components/PizzaMenu/PizzaMenu.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import menuCart from '../../data/menu';
+// import menuCart from '../../data/menu';
 import PizzaList from '../PizzaList/PizzaList';
 
 import './PizzaMenu.scss';
@@ -9,14 +9,30 @@ import './PizzaMenu.scss';
 function PizzaMenu({ history }) {
     useEffect(() => window.scrollTo(0,0), []);
 
-    const bianca = [];
-    const rossa = [];
-    menuCart.map(el => el.category === 'rossa' ? rossa.push(el) : bianca.push(el));
+    const[list, setList] = useState([]);
+  
+    const fetchMenu = async() => { 
+        try {
+          let data = await fetch('http://localhost:5000/api/v1/menu/', {
+          method: 'GET',
+          });
+    
+          data = await data.json();
+          setList(data.data)
+          
+        } catch(err) {
+          console.log(err);
+        }
+      }
+
+      useEffect(() => {
+        fetchMenu();
+      }, [])
 
     return (
         <div className="container container--menu">
-            <PizzaList list={rossa} title="Pizza Rossa" />
-            <PizzaList list={bianca} title="Pizza Bianca" />
+            <PizzaList list={list} title="Pizza Rossa" />
+            <PizzaList list={list} title="Pizza Bianca" />
             <div className="redirection">
                 <p className="redirection__description" >Nie zanalazłeś pizzy na jaką masz ochotę?</p>
                 <button className="redirection__button"onClick={() => history.push('/mamma_mia/compose')}>
