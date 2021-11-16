feb7399720a0b8a98f8407de4271626b30568fcc
jmczerwinska <czerwinska.jm@gmail.com>
2021-11-16 13:46:38 +0100

    rebuild delivery form with react -hook-form

--- client/src/components/Delivery/DeliveryForm.jsx
+++ client/src/components/Delivery/DeliveryForm.jsx
@@ -1,139 +1,115 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { withRouter } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import { ContextConsumer } from '../..';
 import './DeliveryForm.scss';
 
 function DeliveryForm({ history }) {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [street, setStreet] = useState('');
-    const [buildingNumber, setBuildingNumber] = useState('');
-    const [localNumber, setLocalNumber] = useState('');
-    const [comment, setComment] = useState('');
-
-    const collectDeliveryData = (context) => {
-        const data = {
-            firstName,
-            lastName,
-            street,
-            buildingNumber,
-            localNumber,
-            comment
-        };
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
         console.log(data);
-        context.refresh([]);
+
+        //TODO skasować koszyk
 
         history.push("/mamma_mia/order/summary");
-    }
+    };
 
     return (
-        <ContextConsumer>
-            {context => (
-                <form
-                    className="delivery-form"
-                    onSubmit={() => collectDeliveryData(context)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
 
-                    <fieldset className="delivery-form__group">
-                        <legend className="delivery-form__title">Dane do dostawy</legend>
-                        <div className="wrapper">
-                            <fieldset className="delivery-form__group delivery-form__group--single">
-                                <label
-                                    className="delivery-form__label"
-                                    htmlFor="first-name">Imię</label>
-                                <input
-                                    className="delivery-form__input"
-                                    name="first-name"
-                                    type="text"
-                                    placeholder="Imię*"
-                                    onChange={e => setFirstName(e.target.value)} required />
-                            </fieldset>
-                            <fieldset className="delivery-form__group  delivery-form__group--single">
-                                <label
-                                    className="delivery-form__label"
-                                    htmlFor="last-name">Nazwisko</label>
-                                <input
-                                    className="delivery-form__input"
-                                    name="last-name"
-                                    type="text"
-                                    placeholder="Nazwisko*"
-                                    onChange={e => setLastName(e.target.value)}
-                                    required />
-                            </fieldset>
-                        </div>
+            <fieldset className="delivery-form__group">
+                <legend className="delivery-form__title">Dane do dostawy</legend>
+
+                <label htmlFor="name" className="delivery-form__label">Imię i Nazwisko</label>
+                <input
+                    name="name"
+                    placeholder="Imię i Nazwisko*"
+                    className="delivery-form__input"
+                    {...register("name")} />
+
+                <div className="wrapper">
+                    <fieldset className="delivery-form__group  delivery-form__group--single">
+                        <label htmlFor="email" className="delivery-form__label">E-mail</label>
+                        <input
+                            name="email"
+                            placeholder="E-mail*"
+                            className="delivery-form__input"
+                            {...register("email")} />
                     </fieldset>
 
-                    <fieldset className="delivery-form__group">
-                        <legend className="delivery-form__title">Adres</legend>
-                        <div className="wrapper">
-                            <fieldset className="delivery-form__group delivery-form__group--single">
-                                <label
-                                    className="delivery-form__label"
-                                    htmlFor="street">Ulica</label>
-                                <input
-                                    className="delivery-form__input"
-                                    name="street"
-                                    type="text"
-                                    placeholder="Ulica*"
-                                    onChange={e => setStreet(e.target.value)}
-                                    required />
-                            </fieldset>
-
-                            <div className="wrapper wrapper--numbers">
-                                <fieldset className="delivery-form__group
-                                delivery-form__group--number">
-                                    <label
-                                        className="delivery-form__label"
-                                        htmlFor="building-no">Nr domu</label>
-                                    <input
-                                        className="delivery-form__input"
-                                        name="building-no"
-                                        type="text"
-                                        placeholder="Nr budynku*"
-                                        onChange={e => setBuildingNumber(e.target.value)}
-                                        required />
-                                </fieldset>
-                                <fieldset className="delivery-form__group delivery-form__group--number">
-                                    <label
-                                        className="delivery-form__label"
-                                        htmlFor="flat-no">Nr lokalu</label>
-                                    <input
-                                        className="delivery-form__input"
-                                        name="flat-no"
-                                        text="text"
-                                        placeholder="Nr lokalu"
-                                        onChange={e => setLocalNumber(e.target.value)} />
-                                </fieldset>
-                            </div>
-                        </div>
+                    <fieldset className="delivery-form__group  delivery-form__group--single">
+                        <label htmlFor="phone" className="delivery-form__label">Telefon</label>
+                        <input
+                            name="phone"
+                            placeholder="Telefon*"
+                            className="delivery-form__input"
+                            {...register("phoneNumber")} />
                     </fieldset>
-                    <fieldset className="delivery-form__group">
-                        <legend className="delivery-form__title">Komentarz do zamówienia</legend>
-                        <textarea
-                            className="delivery-form__input delivery-form__input--text"
-                            rows="5"
-                            onChange={e => setComment(e.target.value)}
-                            placeholder="Uwagi dotyczące zamówienia..." />
-                    </fieldset>
+                </div>
 
-                    <div className="button-group">
-                        <button
-                            type="button"
-                            className="button button--back"
-                            onClick={() => history.push('/mamma_mia/order/basket')}>
-                            Wstecz
-                        </button>
-                        <button
-                            type="submit"
-                            className="button button--next"
-                            onClick={() => collectDeliveryData(context)}>
-                            Zamów
-                        </button>
-                    </div>
-                </form>
-            )}
-        </ContextConsumer>
+            </fieldset>
+
+            <fieldset className="delivery-form__group">
+                <label htmlFor="city" className="delivery-form__label">Miasto</label>
+                <input 
+                    name="city"
+                    placeholder="Miasto*"
+                    className="delivery-form__input"
+                    {...register("city")} />
+            </fieldset>
+
+            <div className="wrapper">
+                <fieldset className="delivery-form__group delivery-form__group--single">
+                    <label htmlFor="street" className="delivery-form__label">Ulica</label>
+                    <input 
+                        name="street"
+                        placeholder="Ulica*"
+                        className="delivery-form__input"
+                        {...register("street")} />
+                </fieldset>
+
+                <div className="wrapper wrapper--numbers">
+                    <fieldset className="delivery-form__group delivery-form__group--number">
+                        <label htmlFor="buildingNumber" className="delivery-form__label">Numer domu</label>
+                        <input
+                            name="buildingNumber"
+                            placeholder="Numer domu"
+                            className="delivery-form__input"
+                            {...register("buildingNumber")} />
+                    </fieldset>
+                    <fieldset className="delivery-form__group delivery-form__group--number">
+                        <label htmlFor="localNumber" className="delivery-form__label">Numer lokalu</label>
+                        <input 
+                            name="localNumber"
+                            placeholder="Numer lokalu" 
+                            className="delivery-form__input"
+                            {...register("localNumber")} />
+                    </fieldset>
+                </div>
+                
+            </div>
+
+            <fieldset className="delivery-form__group">
+                <legend className="delivery-form__title">Komentarz do zamówienia</legend>
+                <textarea
+                    className="delivery-form__input delivery-form__input--text"
+                    rows="5"
+                    placeholder="Uwagi dotyczące zamówienia..."
+                    {...register("comment")} />
+            </fieldset>
+
+            <div className="button-group">
+                <button
+                    type="button"
+                    className="button button--back"
+                    onClick={() => history.push('/mamma_mia/order/basket')}>
+                    Wstecz
+                </button>
+                <input type="submit" value="Zamów" className="button button--next" />
+            </div>
+            
+        </form >
     )
 }
 
