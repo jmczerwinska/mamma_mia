f1e7900c5dd10fdbe0141c4761d89edcf1de6248
jmczerwinska <czerwinska.jm@gmail.com>
2021-11-30 20:02:37 +0100

    add new keys to  order schema

--- server/models/Order.js
+++ server/models/Order.js
@@ -5,23 +5,49 @@ const OrderSchema = mongoose.Schema({
         type: Number,
         default: Date.now
     },
+    client: {
+        type: String,
+        required: [true, 'Add your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Add e-mail']
+    },
+    phone: {
+        type: String,
+        required: [true, 'Add phone number']
+    },
     status: {
         type: String,
         required: [true, 'Please add order status'],
-        enum: ['accepted', 'in progress', 'completed', 'canceled']
+        enum: ['accepted', 'in progress', 'completed', 'canceled'],
+        default: 'accepted'
     },
     list: [],
     deliveryAdress: {
-        name: String,
-        street: String,
-        buildingNumber: Number,
+        street: {
+            type: String,
+            required: [true, 'Add street']
+        },
+        buildingNumber: {
+            type: Number,
+            required: [true, 'Add building number']
+        },
         localNumber: Number,
-        city: String
+        city: {
+            type: String,
+            enum: {values: ['Warszawa'], message: 'Delivery awailable only in Warsaw'},
+            required: [true, 'Add city']
+        },
+    },
+    comment: {
+        type: String,
+        required: [true, 'Add comment']
     },
     user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
     }
-}, {timestamps: true});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Order', OrderSchema);
