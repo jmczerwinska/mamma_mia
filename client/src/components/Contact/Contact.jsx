f8cd8aa6d9d165ad63d7058b27fc56f45f5542a4
jmczerwinska <czerwinska.jm@gmail.com>
2021-11-30 21:20:25 +0100

    add padding to contact form section

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhoneAlt, faEnvelopeOpenText, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import ContactForm from '../ContactForm/ContactForm';
 
 import './Contact.scss';
+
 
 function Contact() {
     useEffect(() => window.scrollTo(0, 0), []);
@@ -34,13 +36,11 @@ function Contact() {
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2517.604266249467!2d21.020613087409266!3d52.220407592150906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecce466b29a8f%3A0x894d7567580963af!2sal.%20Ujazdowskie%2C%20Warszawa!5e0!3m2!1spl!2spl!4v1593690143949!5m2!1spl!2spl" className="location__map" aria-hidden="false" title="map"></iframe>
 
             </section>
+            
             <section className="contact-form">
                 <h4>Napisz do nas!</h4>
+                <ContactForm/>
             </section>
-
-
-
-
         </div>
     )
 }
