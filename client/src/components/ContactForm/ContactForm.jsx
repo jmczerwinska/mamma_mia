ad22d99b0131c4ef4ddfe96944a60e26a2371292
jmczerwinska <czerwinska.jm@gmail.com>
2021-12-01 17:20:39 +0100

    change class names add new styles

--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
+
+import './ContactForm.scss';
 
 function ContactForm() {
     const { register, formState: { errors }, handleSubmit } = useForm();
@@ -10,45 +12,46 @@ function ContactForm() {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
 
-            <fieldset className="delivery-form__group">
-                <label htmlFor="name" className={`delivery-form__label`}>Imię i Nazwisko</label>
+            <fieldset className="contact-form__group">
+                <label htmlFor="name" className={`contact-form__label`}>Imię i Nazwisko</label>
                 <input
                     name="name"
                     placeholder="Imię i Nazwisko*"
-                    className="delivery-form__input"
+                    className="contact-form__input"
                     {...register("name", { required: true })} />
             </fieldset>
 
-            <fieldset className="delivery-form__group">
-                <label htmlFor="email" className="delivery-form__label">E-mail</label>
+            <fieldset className="contact-form__group">
+                <label htmlFor="email" className="contact-form__label">E-mail</label>
                 <input
                     name="email"
                     placeholder="E-mail*"
-                    className="delivery-form__input"
+                    className="contact-form__input"
                     {...register("email", {
                         required: true,
                         pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                     })} />
             </fieldset>
 
-            <fieldset className="delivery-form__group">
-                <label htmlFor="title" className={`delivery-form__label`}>Tytuł</label>
+            <fieldset className="contact-form__group">
+                <label htmlFor="title" className={`contact-form__label`}>Tytuł</label>
                 <input
                     name="title"
                     placeholder="Tytuł*"
-                    className="delivery-form__input"
+                    className="contact-form__input"
                     {...register("title", { required: true })} />
             </fieldset>
 
-            <fieldset className="delivery-form__group">
-                <label htmlFor="message" className={`delivery-form__label`}>Wiadomość</label>
+            <fieldset className="contact-form__group">
+                <label htmlFor="message" className={`contact-form__label`}>Wiadomość</label>
                 <textarea
-                    className="delivery-form__input delivery-form__input--text"
+                    className="contact-form__input contact-form__input--text"
                     rows="5"
                     placeholder="Miejsce na Twoją wiadomość..."
                     {...register("message", { required: true })} />
             </fieldset>
-            <input type="submit" value="Wyślij" className="button button--next" />
+            
+            <input type="submit" value="Wyślij" className="button button--send" />
 
         </form>
     )
