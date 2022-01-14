11e0711dbb0e0406e673a3978552e11ae90a81a8
jmczerwinska <czerwinska.jm@gmail.com>
2022-01-14 12:14:15 +0100

    hide and style review form

--- client/src/components/ReviewForm/ReviewForm.jsx
+++ client/src/components/ReviewForm/ReviewForm.jsx
@@ -1,56 +1,62 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import StarRating from '../StarRating/StarRating';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import StarRating from "../StarRating/StarRating";
 
-import './ReviewForm.scss';
-
+import "./ReviewForm.scss";
 
 function ReviewForm() {
-    const { register, watch, handleSubmit } = useForm();
-    const watchRating = watch('rating', null);
-    const [hover, setHover] = useState(null);
+  const { register, watch, handleSubmit } = useForm();
+  const watchRating = watch("rating", null);
+  const [hover, setHover] = useState(null);
 
-    return (
-        <form onSubmit={handleSubmit(data => console.log(data))}>
-            <div className="wrapper">
-                <fieldset className="contact-form__group">
-                    <label htmlFor="name" className={`contact-form__label`}>Imię i Nazwisko</label>
-                    <input
-                        name="name"
-                        placeholder="Imię i Nazwisko*"
-                        className="contact-form__input"
-                        {...register("name", { required: true })} />
-                </fieldset>
+  return (
+    <form
+      className="review-form"
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
+      <fieldset className="review-form__group review-form__group--rating">
+        <legend className="review-form__legend">Ocena:</legend>
+        {[...Array(5)].map((el, i) => {
+          const ratingValue = i + 1;
+          return (
+            <StarRating
+              key={i}
+              register={register}
+              ratingValue={ratingValue}
+              watchRating={watchRating}
+              setHover={setHover}
+              hover={hover}
+            />
+          );
+        })}
+      </fieldset>
+      <fieldset className=" review-form__group review-form__group--name">
+        <label htmlFor="name" className="review-form__label">
+          Imię
+        </label>
+        <input
+          name="name"
+          placeholder="Imię i Nazwisko*"
+          className="review-form__input"
+          {...register("name", { required: true })}
+        />
+      </fieldset>
 
-                <fieldset className="contact-form__group">
-                    <legend>Ocena</legend>
-                    {[...Array(5)].map((el, i) => {
-                        const ratingValue = i + 1;
-                        return (
-                            <StarRating
-                                key={i}
-                                register={register}
-                                ratingValue={ratingValue}
-                                watchRating={watchRating}
-                                setHover={setHover}
-                                hover={hover} />
-                        )
-                    })}
-                </fieldset>
+      <fieldset className="review-form__group">
+        <label htmlFor="text" className="review-form__label">
+          Twoja opinia
+        </label>
+        <textarea
+          className="review-form__input review-form__input--text"
+          rows="5"
+          placeholder="Miejsce na Twoją opinię..."
+          {...register("text", { required: true })}
+        />
+      </fieldset>
 
-            </div>
-
-            <fieldset className="contact-form__group">
-                <label htmlFor="text" className={`contact-form__label`}>Wiadomość</label>
-                <textarea
-                    className="contact-form__input contact-form__input--text"
-                    rows="5"
-                    placeholder="Miejsce na Twoją wiadomość..."
-                    {...register("text", { required: true })} />
-            </fieldset>
-            < input type="submit" value="Zapisz" />
-        </form>
-    )
+      <input className="button" type="submit" value="Zapisz" />
+    </form>
+  );
 }
 
 export default ReviewForm;
--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,37 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import ReviewForm from '../ReviewForm/ReviewForm';
-import SingleReview from '../SingleReview/SingleReview';
-
+import React, { useState, useEffect } from "react";
+import ReviewForm from "../ReviewForm/ReviewForm";
+import SingleReview from "../SingleReview/SingleReview";
 
 function Reviews() {
-    const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
-    const fetchreviews = async () => {
-        try {
-            const data = await fetch('http://localhost:5000/api/v1/reviews', {
-                method: 'GET',
-            });
+  const fetchReviews = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/v1/reviews/", {
+        method: "GET",
+      });
 
-            const parseData = await data.json();
+      const parseResponse = await response.json();
 
-            setReviews(parseData.data);
+      if (!parseResponse.success) {
+        throw new Error(`${response.status} - ${parseResponse.message}`);
+      }
 
-        } catch (err) {
-            console.log(err);
-        }
+      setReviews(parseResponse.data);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(() => {
-        fetchreviews();
-    }, [])
+  useEffect(() => {
+    fetchReviews();
+  }, []);
 
-    return (
-        <div className="container container--reviews">
-            <ReviewForm />
-            {reviews.map((el, i) => <SingleReview key={i} review={el} />)}
-        </div>
-
-    )
+  return (
+    <div className="container container--reviews">
+      <button onClick={()=> setShowForm(prevState => !prevState)}>{showForm ? "Ukryj formularz" : "Dodaj opinie"}</button>
+      {showForm ? <ReviewForm /> : null}
+      {reviews.map((el, i) => (
+        <SingleReview key={i} review={el} />
+      ))}
+    </div>
+  );
 }
 
 export default Reviews;
