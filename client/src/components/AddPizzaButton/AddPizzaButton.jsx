6dbc8bfb90b229d8270de1b2c25caa15f869d0b3
jmczerwinska <czerwinska.jm@gmail.com>
2022-01-14 10:29:36 +0100

    remove toast import

--- client/src/components/AddPizzaButton/AddPizzaButton.jsx
+++ client/src/components/AddPizzaButton/AddPizzaButton.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { toast } from "react-toastify";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
