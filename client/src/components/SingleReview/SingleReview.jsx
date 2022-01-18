4b6220cfc4215f875e896b23c0fa1a503b2c3026
jmczerwinska <czerwinska.jm@gmail.com>
2022-01-18 22:20:22 +0100

    add stars rating and some css styles

--- client/src/components/SingleReview/SingleReview.jsx
+++ client/src/components/SingleReview/SingleReview.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import "./SingleReview.scss";
 
 function SingleReview({ review }) {
-    const { title, rating, text } = review;
+  const { title, rating, text } = review;
 
-    return (
-        <div>
-            <h1>{title}</h1>
-            <h2>Ocena: {rating}/5</h2>
-            <p>{text}</p>
-        </div>
-    )
+  return (
+    <div className="review">
+      <h3 className="review__title">{title}</h3>
+      <h4 className="review__rating">
+        {rating}/5
+        <span className="review__stars">
+          {[...Array(5)].map((el, i) => {
+            const ratingValue = i + 1;
+            return (
+              <FontAwesomeIcon
+                key={ratingValue}
+                className="rating__star"
+                icon={faStar}
+                color={ratingValue <= rating ? "#ffc400" : "lightGrey"}
+              />
+            );
+          })}
+        </span>
+      </h4>
+
+      <p className="review__text">{text}</p>
+    </div>
+  );
 }
 
 export default SingleReview;
