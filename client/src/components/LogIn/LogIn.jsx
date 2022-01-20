66c5f04453ff990b094b3c6588325263ac569a02
jmczerwinska <czerwinska.jm@gmail.com>
2022-01-20 22:05:18 +0100

    add css styles to auth form

--- client/src/components/LogIn/LogIn.jsx
+++ client/src/components/LogIn/LogIn.jsx
@@ -1,17 +1,35 @@
 import React from "react";
+import "./LogIn.scss";
 
 function LogIn() {
   return (
-    <div>
-      <form>
-        <input type="text" name="email" />
-        <input type="password" name="passwword" />
+    <div className="inner-box">
+      <form className="auth-form">
+        <input
+          type="text"
+          placeholder="E-mail"
+          name="email"
+          className="auth-form__input"
+        />
+        <input
+          type="password"
+          placeholder="Hasło"
+          name="passwword"
+          className="auth-form__input"
+        />
         <input type="submit" value="Zaloguj" className="button" />
       </form>
-      <p>Zapomniałeś hasła?</p>
-      <p>Nie masz jeszcze konta? <a>Załóż je!</a></p>
+      <a href="#" className="switch-link switch-link--muted">
+        Zapomniałeś hasła?
+      </a>
+      <p>
+        Nie masz jeszcze konta?
+        <a href="#" className="switch-link switch-link--bold">
+          Załóż je!
+        </a>
+      </p>
     </div>
-    );
+  );
 }
 
 export default LogIn;
