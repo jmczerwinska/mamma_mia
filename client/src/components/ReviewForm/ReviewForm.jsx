5eab45b7593f2b4e246da042c633c00dfda1e613
jmczerwinska <czerwinska.jm@gmail.com>
2022-02-04 21:04:11 +0100

    add toasts and close button

--- client/src/components/ReviewForm/ReviewForm.jsx
+++ client/src/components/ReviewForm/ReviewForm.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
 import StarRating from "../StarRating/StarRating";
 import ValidationMessage from "../ValidationMessage/ValidationMessage";
 
 import "./ReviewForm.scss";
 
-function ReviewForm() {
+function ReviewForm({ closeModal }) {
   const {
     register,
     watch,
@@ -32,62 +36,70 @@ function ReviewForm() {
       if (!parseResponse.success) {
         throw new Error(`${response.status} - ${parseResponse.message}`);
       }
+
+      toast.success("Dodano komentarz!");
       console.log(parseResponse);
     } catch (err) {
+      toast.error(err);
       console.log(err);
     }
   };
 
   return (
-    <form
-      className="review-form"
-      onSubmit={handleSubmit((data) => sendReview(data))}
-    >
-      {Object.entries(errors).some((err) => err[1].type === "required") && (
-        <ValidationMessage message={"Uzupełnij brakujące pola"} />
-      )}
-      <fieldset className="review-form__group review-form__group--rating">
-        <legend className="review-form__legend">Ocena:</legend>
-        {[...Array(5)].map((el, i) => {
-          const ratingValue = i + 1;
-          return (
-            <StarRating
-              key={i}
-              register={register}
-              ratingValue={ratingValue}
-              watchRating={watchRating}
-              setHover={setHover}
-              hover={hover}
-            />
-          );
-        })}
-      </fieldset>
-      <fieldset className=" review-form__group review-form__group--name">
-        <label htmlFor="title" className="review-form__label">
-          Tytuł
-        </label>
-        <input
-          title="tytuł"
-          placeholder="Tytuł"
-          className="review-form__input"
-          {...register("title", { required: true })}
-        />
-      </fieldset>
+    <div className="box"  onClick={(e) => e.stopPropagation()}>
+      <button className="exit-btn" onClick={closeModal}>
+        <FontAwesomeIcon icon={faTimes} className="exit-btn__icon" />
+      </button>
+      <form
+        className="review-form"
+        onSubmit={handleSubmit((data) => sendReview(data))}
+      >
+        {Object.entries(errors).some((err) => err[1].type === "required") && (
+          <ValidationMessage message={"Uzupełnij brakujące pola"} />
+        )}
+        <fieldset className="review-form__group review-form__group--rating">
+          <legend className="review-form__legend">Ocena:</legend>
+          {[...Array(5)].map((el, i) => {
+            const ratingValue = i + 1;
+            return (
+              <StarRating
+                key={i}
+                register={register}
+                ratingValue={ratingValue}
+                watchRating={watchRating}
+                setHover={setHover}
+                hover={hover}
+              />
+            );
+          })}
+        </fieldset>
+        <fieldset className=" review-form__group review-form__group--name">
+          <label htmlFor="title" className="review-form__label">
+            Tytuł
+          </label>
+          <input
+            title="tytuł"
+            placeholder="Tytuł"
+            className="review-form__input"
+            {...register("title", { required: true })}
+          />
+        </fieldset>
 
-      <fieldset className="review-form__group">
-        <label htmlFor="text" className="review-form__label">
-          Twoja opinia
-        </label>
-        <textarea
-          className="review-form__input review-form__input--text"
-          rows="5"
-          placeholder="Miejsce na Twoją opinię..."
-          {...register("text", { required: true })}
-        />
-      </fieldset>
+        <fieldset className="review-form__group">
+          <label htmlFor="text" className="review-form__label">
+            Twoja opinia
+          </label>
+          <textarea
+            className="review-form__input review-form__input--text"
+            rows="5"
+            placeholder="Miejsce na Twoją opinię..."
+            {...register("text", { required: true })}
+          />
+        </fieldset>
 
-      <input className="button" type="submit" value="Zapisz" />
-    </form>
+        <input className="button" type="submit" value="Zapisz" />
+      </form>
+    </div>
   );
 }
 
