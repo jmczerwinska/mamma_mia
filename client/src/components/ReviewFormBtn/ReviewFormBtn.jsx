b52f274e0b87ddd3a3bb4d25e4c8023257284ce8
jmczerwinska <czerwinska.jm@gmail.com>
2022-02-09 19:01:59 +0100

    remove useContext import

--- client/src/components/ReviewFormBtn/ReviewFormBtn.jsx
+++ client/src/components/ReviewFormBtn/ReviewFormBtn.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import Modal from "../Modal/Modal";
 import ReviewForm from "../ReviewForm/ReviewForm";
 
